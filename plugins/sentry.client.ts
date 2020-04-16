99aa4947da93122f8e36d7a813cb664959decb15
yu <112287812@qq.com>
2020-04-16 23:09:07 +0800

    modify code

--- plugins/sentry.client.ts
+++ plugins/sentry.client.ts
@@ -1,8 +1,8 @@
-import Raven from 'raven-js';
-import Vue from 'vue';
+import Raven from 'raven-js'
+import Vue from 'vue'
 
-Raven.config('https://b61abe8131f74cb09330266225caba4f@report.url.cn/sentry/203').install();
+Raven.config('https://b61abe8131f74cb09330266225caba4f@report.url.cn/sentry/203').install()
 
 Vue.config.errorHandler = (err, vm, info) => {
-    Raven.captureException(err);
-};
+    Raven.captureException(err)
+}
